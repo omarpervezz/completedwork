bd89bba6240051f46695aab40b327cd87231809f
Omar Pervez <70445442+omarpervezz@users.noreply.github.com>
2022-03-16 14:56:24 +0600

    I have done the owrk

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,16 @@ const pushbar = new Pushbar({
 pushbar.open("mypushbar1");
 //close all pushbars
 pushbar.close();
+
+// mobile menu sab collpase
+const collpaseMenu = document.getElementById('collapse_menu');
+const expandCollapse = collpaseMenu.querySelector('.collpaseMenu');
+console.log(expandCollapse)
+const mobileMenuC = document.querySelector('#mobile-sub-menu');
+
+
+collpaseMenu.addEventListener(('click'),  () => {
+  mobileMenuC.classList.toggle('collapseMobileMenu');
+  expand.classList.add('rotate');
+  
+})
